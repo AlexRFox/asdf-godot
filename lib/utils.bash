8fbbaf3133bf1687c9f0b434589cb91d20439afc
Alex Fox <atw@alum.mit.edu>
2024-08-11 16:00:48 -0400

    test

--- lib/utils.bash
+++ lib/utils.bash
@@ -72,10 +72,10 @@ download_release() {
   release=`echo "$1" | cut -d'-' -f2`
   platform="$3"
   url="$GH_BUILDS_REPO/$1/Godot_v$1_${platform}.zip"
-  fail $url
 
   echo "* Downloading $TOOL_NAME release $1..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  fail "foo"
 }
 
 
