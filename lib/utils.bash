82ac8120ad28fccc24aab720dad5cdb8486e3a57
Alex Fox <atw@alum.mit.edu>
2024-08-12 00:54:29 -0400

    x

--- lib/utils.bash
+++ lib/utils.bash
@@ -75,6 +75,7 @@ download_release() {
 
   echo "* Downloading $TOOL_NAME release $1..."
   echo $url
+  echo curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
